8f96369dca950da3397c389c699e54605214f890
Juan Castrillon <juandicastri@gmail.com>
2014-01-16 20:24:02 -0500

    new election js

---
+++ public/js/newElection.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	var candidateID = 1;
+
+	$(document).on('click', '.removeCandidate', function() {
+		var id = $(this).attr('id').substring('removeBtn'.length);
+		$('#newCandidate' + id).remove();
+		$('#removeBtn' + id).remove();
+	});
+
+	$('.addCandidate').click(function() {
+		var newCand = '<input id="newCandidate' + candidateID + '" class="form-control" type="text" name="candidates" placeholder="new candidate">';
+		var remCandBtn = '<span class="removeCandidate glyphicon glyphicon-minus-sign" id="removeBtn' + candidateID + '">';
+		var newRow = newCand + remCandBtn;
+		$(newRow).insertBefore('.submit');
+		candidateID = candidateID + 1;
+	});
+
+});
